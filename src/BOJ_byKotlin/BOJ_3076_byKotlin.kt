6a37bfdc5482e27e6086f297ec3d5c708a7f92a7
conatuseus <conatuseus@gmail.com>
2018-12-23 21:27:45 +0900

    Add Solution of 10942

--- src/BOJ_byKotlin/BOJ_3076_byKotlin.kt
+++ src/BOJ_byKotlin/BOJ_3076_byKotlin.kt
@@ -8,21 +8,6 @@ fun main(args: Array<String>) {
     val (r,c)=br.readLine()!!.split(' ').map { it.toInt() }
     val (a,b)=br.readLine()!!.split(' ').map { it.toInt() }
 
-//    fun insert(x:Int,y:Int,type:Int){
-//        for(i in x until x+a){
-//            for(j in y until y+b){
-//                map[i][j]=if(type==1)  'X' else '.'
-//            }
-//        }
-//    }
-//
-//    var type=1
-//    for(i in 0 until r*a step a){
-//        for(j in 0 until c*b step b){
-//            insert(i,j,type)
-//            type*=-1
-//        }
-//    }
     for(i in 0 until r*a){
         for(j in 0 until c*b){
             if(i%(a*2) < a == j%(b*2) < b)  print("X")
