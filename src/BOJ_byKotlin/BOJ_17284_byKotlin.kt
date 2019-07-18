bb643163353cce879ec532f58c093bd991b2e7e4
conatuseus <conatuseus@gmail.com>
2019-07-18 21:51:30 +0900

    Add Solution of 17284

--- src/BOJ_byKotlin/BOJ_17284_byKotlin.kt
+++ src/BOJ_byKotlin/BOJ_17284_byKotlin.kt
@@ -1,14 +1,3 @@
 package BOJ_byKotlin
 
-fun main() {
-    val str= readLine()!!.split(' ').map { it.toInt() }
-    var res=5000
-    for (i in 0 until str.size){
-        res-=when(str[i]){
-            1-> 500
-            2-> 800
-            else -> 1000
-        }
-    }
-    println(res)
-}
+fun main() { println(5000-readLine()!!.split(' ').map{if(it=="1") 500 else if(it=="2") 800 else 1000 }.sum()) }
