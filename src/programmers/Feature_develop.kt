8227ab4cd55e0ac7eb83c11eb9d6b449bf2f5169
MK S <conatuseus@gmail.com>
2019-09-05 23:57:52 +0900

    Solution of Feature_develop

---
+++ src/programmers/Feature_develop.kt
@@ -0,0 +1,27 @@
+package programmers
+
+class Solution {
+    fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+        var answer = mutableListOf<Int>()
+
+        var days = 101
+        var count = 0
+        for (i in 0 until progresses.size) {
+            val nowCountDays =
+                if ((100 - progresses[i]) % speeds[i] == 0) (100 - progresses[i]) / speeds[i] else ((100 - progresses[i]) / speeds[i] )+ 1
+            if (days >= nowCountDays) {
+                if(days==101)   days=nowCountDays
+                count++
+            } else {
+                days=nowCountDays
+                answer.add(count)
+                count = 1
+            }
+            if(i==progresses.size-1){
+                answer.add(count)
+            }
+        }
+
+        return answer.toIntArray()
+    }
+}
