1748119cd726953f6e4ba67d1590b0c801b7ce7b
MK S <conatuseus@gmail.com>
2020-02-15 21:32:29 +0900

    프로그래머스 SQL <오랜 기간 보호한 동물(1)>

---
+++ src/programmers/sql문제/JOIN문제/protect_long_time.sql
@@ -0,0 +1,5 @@
+SELECT ins.NAME, ins.DATETIME FROM ANIMAL_INS ins
+    LEFT JOIN ANIMAL_OUTS outs ON ins.ANIMAL_ID=outs.ANIMAL_ID
+    WHERE outs.DATETIME is null
+    ORDER BY ins.DATETIME
+    limit 3
